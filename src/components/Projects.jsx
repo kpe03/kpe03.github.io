503978e14aba4d4ebe0b1d0079e510b50f660a6c
Kpeters <kaitlynpeters@cox.net>
2023-10-22 00:19:42 -0500

    edit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,10 +4,15 @@ import { Link } from "react-router-dom";
 import { ProjectWrapper } from "./ProjectsStyledComponents";
 import { MainContent } from "./HomeStyledComponents";
 
+export const projectBuilder(project) => {
+    
+}
+
+
 export const Projects = () => (
     <ProjectWrapper>
         <MainContent>
-            
+            <SectionTitle></SectionTitle>
         </MainContent>
     </ProjectWrapper>
 )
